fbb84711f6449695e92724d635312d478d1b116c
Jacob Boomgaarden <jcboomgaarden@gmail.com>
2017-11-02 20:12:28 +0200

    imported new languages.css file

--- src/components/Languages/Languages.js
+++ src/components/Languages/Languages.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
+import './Languages.css';
 import { changeLanguage } from '../../actions/languageActions';
 
 @connect()
