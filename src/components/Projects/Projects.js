5a00293dd29ffde5b06d1563aee264d7c232d970
Jacob Boomgaarden <jcboomgaarden@gmail.com>
2017-11-03 12:09:46 +0200

    began setting up layout for projects component & styling

---
+++ src/components/Projects/Projects.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './Projects.css';
+
+export default class Projects extends React.Component {
+
+  render() {
+    return (
+      <div className='projects-container'>
+        <div className='projects'>
+          HEY HEY HEY
+        </div>
+      </div>
+    )
+  }
+}
