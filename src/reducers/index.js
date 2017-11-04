935b154a975bc635cfcfb532925abb74ac8f31e4
Jacob Boomgaarden <jcboomgaarden@gmail.com>
2017-11-04 13:31:06 +0200

    added bitbucket & github to reducer object

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,7 +1,11 @@
 import { combineReducers } from 'redux';
 
 import { languageChange } from './languageReducer';
+import { bitBucketInfo } from './bitBucketReducer';
+import { githubInfo } from './githubReducer';
 
 export default combineReducers({
   languageChange,
+  bitBucketInfo,
+  githubInfo,
 });
