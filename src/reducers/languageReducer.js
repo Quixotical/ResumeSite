d207d734ff3d9462d7b43d84bdb45b8ca8aedd49
Jacob Boomgaarden <jcboomgaarden@gmail.com>
2017-11-04 13:31:18 +0200

    removed log statement

--- src/reducers/languageReducer.js
+++ src/reducers/languageReducer.js
@@ -1,7 +1,6 @@
 export const languageChange = (state={language: 'English'}, action) => {
   switch (action.type) {
     case "CHANGE_LANGUAGE": {
-      console.log(...state);
       return {...state, language: action.payload.language}
     }
   }
